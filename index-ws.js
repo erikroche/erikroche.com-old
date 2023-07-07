e3610d04c1eb1b737d40ad7d98791f2737cc604a
Erik  Roche <erikroche@Eriks-Air.station>
2023-07-07 13:13:30 +0100

    Created database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,13 @@ server.on('request', app);
 
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        shutdownDB();
+    })
+})
+
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
 
@@ -26,6 +33,10 @@ wss.on('connection', function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send('welcome!');
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
 
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
@@ -49,3 +60,28 @@ wss.broadcast = function broadcast(data) {
   });
 };
 /** End Websocket **/
+/** Begin Database */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    })
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log("Shutting down DB");
+    db.close();
+}
